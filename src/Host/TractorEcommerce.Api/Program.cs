bceacb50eb3477c5d499bb7fd78457e08ce31344
JesusJs <jdchavezzss@gmail.com>
2026-05-23 15:32:46 -0400

    Resolucion de problemas en salesDbContext.cs

--- src/Host/TractorEcommerce.Api/Program.cs
+++ src/Host/TractorEcommerce.Api/Program.cs
@@ -4,6 +4,33 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("MfeCorsPolicy", policy =>
+    {
+        policy.AllowAnyOrigin() // En producción cambiar por dominios específicos
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
+    ?? "Host=localhost;Database=tractor_ecommerce_db;Username=tractor_admin;Password=tractor_password";
+
+builder.Services.AddDbContext<CatalogDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+// 3. Registrar Infraestructura Compartida
+builder.Services.AddSingleton<IEventBus, KafkaEventBus>();
+
+// 4. Agregar Seguridad (JWT)
+builder.Services.AddJwtAuthentication(builder.Configuration);
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -32,6 +59,11 @@ app.MapGet("/weatherforecast", () =>
     return forecast;
 })
 .WithName("GetWeatherForecast");
+app.UseCors("MfeCorsPolicy");
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
 
