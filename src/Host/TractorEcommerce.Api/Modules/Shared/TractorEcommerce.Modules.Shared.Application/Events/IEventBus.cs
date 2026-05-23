728df53e0f5990ac451268a66c48780883597e2f
JesusJs <jdchavezzss@gmail.com>
2026-05-23 18:28:20 -0500

    Implementando metodos compartidos del kafka para el manejo de eventos

---
+++ src/Host/TractorEcommerce.Api/Modules/Shared/TractorEcommerce.Modules.Shared.Application/Events/IEventBus.cs
@@ -0,0 +1,7 @@
+﻿namespace TractorEcommerce.Api.Modules.Shared.TractorEcommerce.Modules.Shared.Application.Events
+{
+    public interface IEventBus
+    {
+        Task PublishAsync<T>(string topic, string key, T message) where T : class;
+    }
+}
---
+++ src/Host/TractorEcommerce.Api/Modules/Shared/TractorEcommerce.Modules.Shared.Application/Events/OrderPlacedEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace TractorEcommerce.Api.Modules.Shared.TractorEcommerce.Modules.Shared.Application.Events
+{
+    public record OrderPlacedEvent(
+    string OrderId,
+    IEnumerable<OrderEventItem> Items,
+    DateTime OccurredAt
+);
+
+    public record OrderEventItem(string Sku, int Quantity);
+}
