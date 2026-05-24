0e0e2252f03d8f9af9082b160dda2d89a7bd97f1
JesusJs <jdchavezzss@gmail.com>
2026-05-24 18:47:03 -0500

    Implementando el ProductUpdateEvent

---
+++ src/Modules/Catalog/Application/Events/ProductStockUpdatedEvent.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TractorEcommerce.Modules.Catalog.Application.Events
+{
+    public record ProductStockUpdatedEvent(string Sku, int NewStock);
+}
