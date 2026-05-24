5de09cd56f7313b27cff1a2ff4920a65cbdb322a
JesusJs <jdchavezzss@gmail.com>
2026-05-24 11:19:04 -0500

    Implementando interfaz de servicio inventory

---
+++ src/Host/TractorEcommerce.Api/Modules/Shared/TractorEcommerce.Modules.Shared.Application/Ports/IInventoryInternalService.cs
@@ -0,0 +1,9 @@
+﻿namespace TractorEcommerce.Api.Modules.Shared.TractorEcommerce.Modules.Shared.Application.Ports
+{
+    public interface IInventoryInternalService
+    {
+        // Permite que otros módulos consulten el stock actual de forma síncrona y eficiente
+        Task<int> GetStockBySkuAsync(string sku);
+        Task<bool> HasEnoughStockAsync(string sku, int quantity);
+    }
+}
