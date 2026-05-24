910e10bee94a67ea88812561de13ef11a3c47778
JesusJs <jdchavezzss@gmail.com>
2026-05-24 08:22:19 -0500

    Implementacion de un domain para controlar exceptions

---
+++ src/Host/TractorEcommerce.Api/Modules/Shared/TractorEcommerce.Modules.Shared.Application/Exceptions/DomainExceptions.cs
@@ -0,0 +1,21 @@
+﻿namespace TractorEcommerce.Api.Modules.Shared.TractorEcommerce.Modules.Shared.Application.Exceptions
+{
+    public class DomainNotFoundException : Exception
+    {
+        public DomainNotFoundException(string msg) : base(msg) { }
+    }
+
+    public class DomainConflictException : Exception
+    {
+        public DomainConflictException(string msg) : base(msg) { }
+    }
+
+    public class DomainValidationException : Exception
+    {
+        public object Details { get; }
+        public DomainValidationException(string msg, object details) : base(msg)
+        {
+            Details = details;
+        }
+    }
+}
