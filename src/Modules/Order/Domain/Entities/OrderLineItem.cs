86ee01e1ffb93ab8be3ae3f9a774825daea9913d
JesusJs <jdchavezzss@gmail.com>
2026-05-24 18:49:42 -0500

    Entidades deOrder

--- src/Modules/Order/Domain/Entities/OrderLineItem.cs
+++ src/Modules/Order/Domain/Entities/OrderLineItem.cs
@@ -6,6 +6,8 @@ namespace TractorEcommerce.Modules.Order.Domain.Entities
 {
     public record OrderLineItem(Guid Id, string Sku, int Quantity, decimal UnitPrice)
     {
+        public string ProductName;
+
         public decimal Total => UnitPrice * Quantity;
     }
 }
