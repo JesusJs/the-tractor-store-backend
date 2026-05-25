33728a7a48b3510350efacf5d12dfe31582ad744
JesusJs <jdchavezzss@gmail.com>
2026-05-24 21:47:12 -0500

    act

---
+++ src/Modules/Cart/Infrastructure/Data/CartDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TractorEcommerce.Modules.Cart.Domain.Entities;
+
+
+namespace TractorEcommerce.Modules.Cart.Infrastructure.Data
+{
+    public class CartDbContext : DbContext
+    {
+        public CartDbContext(DbContextOptions<CartDbContext> options) : base(options)
+        {
+        }
+
+        // Ruta explícita completa para el DbSet
+        public DbSet<TractorEcommerce.Modules.Cart.Domain.Entities.Cart> Carts => Set<TractorEcommerce.Modules.Cart.Domain.Entities.Cart>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CartDbContext).Assembly);
+        }
+    }
+}
---
+++ src/Modules/Inventory/Infrastructure/Data/InventoryDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Text;
+using TractorEcommerce.Modules.Inventory.Domain.Entities;
+
+namespace TractorEcommerce.Modules.Inventory.Infrastructure.Data
+{
+    public class InventoryDbContext : DbContext
+    {
+        public InventoryDbContext(DbContextOptions<InventoryDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<InventoryItem> Stocks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Carga automáticamente el SkuStockConfiguration
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(InventoryDbContext).Assembly);
+        }
+    }
+}
