56308384f4e49d350cf22bb644155ad51799057d
JesusJs <jdchavezzss@gmail.com>
2026-05-24 21:45:45 -0500

    Act

---
+++ src/Modules/Order/Infrastructure/Data/OrderDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TractorEcommerce.Modules.Order.Domain.Entities;
+
+namespace TractorEcommerce.Modules.Order.Infrastructure.Data
+{
+    public class OrderDbContext : DbContext
+    {
+        public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<CustomerOrder> Orders => Set<CustomerOrder>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Aplica automáticamente todas las configuraciones que implementen IEntityTypeConfiguration en este azmbley
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(OrderDbContext).Assembly);
+        }
+    }
+}
